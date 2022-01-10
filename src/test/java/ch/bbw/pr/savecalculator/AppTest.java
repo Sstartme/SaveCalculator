1fde8ba4b4292f64da10153230d68019825bbaf6
Anpalagan Sinthella, GHR-OSA-NEX-33 <Sinthella.Anpalagan@swisscom.com>
2022-01-10 16:47:28 +0100

    Before and After implementerit

--- src/test/java/ch/bbw/pr/savecalculator/AppTest.java
+++ src/test/java/ch/bbw/pr/savecalculator/AppTest.java
@@ -1,6 +1,8 @@
 package ch.bbw.pr.savecalculator;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.After;
+import org.junit.Before;
 
 import org.junit.Test;
 
@@ -13,6 +15,20 @@ public class AppTest {
     /**
      * Rigorous Test :-)
      */
+
+    @Before
+    public void setup() {
+        SaveCalculator testee = new SaveCalculator();
+        testee = new SaveCalculator();
+    }
+    @After
+    public void deleteSetup() {
+        int value1 = 2;
+        int value2 = 2;
+        SaveCalculator testee = new SaveCalculator();
+        testee.summe(value1, value2);
+    }
+
     @Test
     public void TestSummeZweiPositiveMethodenIstOk() {
         int value1 = 12;
@@ -28,4 +44,6 @@ public class AppTest {
         SaveCalculator testee = new SaveCalculator();
         assertTrue(testee.summe(value1, value2) == -46);
     }
+
+
 }
